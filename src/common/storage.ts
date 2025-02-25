96f20bb98fc93f4afb17b8ca57534410cb1a95ba
sajjad isvand <sajjadzibaee102@gmail.com>
2025-02-25 23:18:30 +0330

    Change storage methods from local to sync for improved data consistency across devices

--- src/common/storage.ts
+++ src/common/storage.ts
@@ -2,11 +2,11 @@ import browser from 'webextension-polyfill'
 import type { StoreKeyType } from './constant/store.key'
 
 export async function setToStorage<T>(key: StoreKeyType, value: T) {
-	browser.storage.local.set({ [key]: value })
+	browser.storage.sync.set({ [key]: value })
 }
 
 export async function getFromStorage<T>(key: StoreKeyType): Promise<T | null> {
-	const value = await browser.storage.local.get(key)
+	const value = await browser.storage.sync.get(key)
 	if (!value) return null
 	return value[key] as T
 }
