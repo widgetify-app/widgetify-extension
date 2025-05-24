a9096238134fc69b98e80e24bd86cb176a445615
Ehsan <ehsanmousavi1.79@gmail.com>
2025-05-24 20:58:20 +0330

    fix(weather): improve RTL language detection and adjust weather text & icon sizes

--- src/layouts/widgets/weather/current/current-box.weather.tsx
+++ src/layouts/widgets/weather/current/current-box.weather.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+import browser from 'webextension-polyfill'
 import Tooltip from '@/components/toolTip'
 import {
 	getBorderColor,
@@ -11,15 +13,43 @@ import { LazyMotion, domAnimation, m } from 'framer-motion'
 import { BsRobot } from 'react-icons/bs'
 import { FaSpotify } from 'react-icons/fa'
 import { WiHumidity, WiStrongWind } from 'react-icons/wi'
+import { IoLocationSharp } from 'react-icons/io5'
 import { unitsFlag } from '../unitSymbols'
 
 interface CurrentWeatherBoxProps {
 	weather: FetchedWeather['weather']
 }
 
+const IsRTLLanguage = async (text: string) => {
+	const tags = ['ar', 'dv', 'fa', 'he', 'ks', 'ku', 'ps', 'sd', 'ug', 'ur', 'yi']
+	let language: string | undefined
+	try {
+		const langInfo = await browser.i18n.detectLanguage(text)
+		language = langInfo.languages[0].language
+	} catch (error) {
+		console.error('Error detecting language rtl:', error)
+	}
+
+	return language ? tags.includes(language) : false
+}
+
 export function CurrentWeatherBox({ weather }: CurrentWeatherBoxProps) {
 	const { weatherSettings, selectedCity } = useWeatherStore()
+	const selectedCityName = selectedCity?.name
 	const { theme } = useTheme()
+	const [isRTLLang, setIsRTLLang] = useState(false)
+
+	useEffect(() => {
+		if (selectedCityName) {
+			IsRTLLanguage(selectedCityName)
+				.then((isRTL) => {
+					setIsRTLLang(isRTL)
+				})
+				.catch((error) => {
+					console.error('Error checking RTL language:', error)
+				})
+		}
+	}, [selectedCityName])
 
 	const getAiIconStyle = () => {
 		switch (theme) {
@@ -88,38 +118,51 @@ export function CurrentWeatherBox({ weather }: CurrentWeatherBoxProps) {
 							</span>
 							{selectedCity?.name && (
 								<div
-									className={`text-sm flex items-center gap-1 truncate ${getTextColor(theme)} transition-colors opacity-90`}
+									className={`text-sm flex items-center gap-1 transition-colors opacity-90 ${getTextColor(theme)} ${
+										!isRTLLang
+											? ' flex-row justify-start '
+											: 'flex-row-reverse justify-end'
+									}`}
 								>
 									<p className="text-xs font-light truncate" title={selectedCity.name}>
 										{selectedCity.name}
 									</p>
+									<span className="h-[16px] opacity-60">
+										<IoLocationSharp />
+									</span>
 								</div>
 							)}
 						</div>
-
-						<img
-							src={weather.icon.url}
-							alt={weather.temperature.temp_description || 'Current weather'}
-							className="w-8 h-8 rounded-full drop-shadow-lg"
-							loading="lazy"
-						/>
+						<div className="flex items-center">
+							<img
+								src={weather.icon.url}
+								alt={weather.temperature.temp_description || 'Current weather'}
+								className="w-8 h-8 rounded-full drop-shadow-lg "
+								loading="lazy"
+							/>
+						</div>
 					</div>
 				</header>
 
-				<div className="flex items-center justify-start mt-2">
+				<div className="flex items-center justify-between mt-2">
 					<div className={`${baseWeatherInfoClass} py-0.5`}>
 						<WiHumidity size={20} className="flex-shrink-0" />
-						<span aria-label={`Humidity: ${weather.temperature.humidity}%`}>
+						<span
+							aria-label={`Humidity: ${weather.temperature.humidity}%`}
+							className="h-[18px]"
+						>
 							{weather.temperature.humidity}%
 						</span>
 					</div>
 					<div className={`${baseWeatherInfoClass} px-2 py-0.5`}>
 						<WiStrongWind size={20} className="flex-shrink-0" />
-						<span
+						<div
+							className="flex items-center gap-1"
 							aria-label={`Wind speed: ${weather.temperature.wind_speed} meters per second`}
 						>
-							{weather.temperature.wind_speed} m/s
-						</span>
+							{weather.temperature.wind_speed}
+							<span className="text-[12px] leading-normal"> m/s </span>
+						</div>
 					</div>
 				</div>
 
