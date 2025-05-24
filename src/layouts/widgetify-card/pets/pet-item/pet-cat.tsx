1497a53a48eae6cb73ffb2aaf66fa0fead98d05c
masihgh <masihgraph@gmail.com>
2025-05-24 18:29:06 +0330

    fix icon and name :D

--- src/layouts/widgetify-card/pets/pet-item/pet-cat.tsx
+++ src/layouts/widgetify-card/pets/pet-item/pet-cat.tsx
@@ -1,9 +1,9 @@
-import idle from '@/assets/animals/cat/akita_idle_8fps.gif'
-import lie from '@/assets/animals/cat/akita_lie_8fps.gif'
-import running from '@/assets/animals/cat/akita_run_8fps.gif'
-import swipe from '@/assets/animals/cat/akita_swipe_8fps.gif'
-import walking from '@/assets/animals/cat/akita_walk_fast_8fps.gif'
-import { LuBone } from 'react-icons/lu'
+import idle from '@/assets/animals/cat/zardaloo_idle_8fps.gif'
+import lie from '@/assets/animals/cat/zardaloo_lie_8fps.gif'
+import running from '@/assets/animals/cat/zardaloo_run_8fps.gif'
+import swipe from '@/assets/animals/cat/zardaloo_swipe_8fps.gif'
+import walking from '@/assets/animals/cat/zardaloo_walk_fast_8fps.gif'
+import { LuMouse } from 'react-icons/lu'
 import { BasePetContainer, useBasePetLogic } from '../core/base-pet'
 import {
 	type PetAnimations,
@@ -42,7 +42,7 @@ export const CatComponent = () => {
 		climb: { min: 2000, max: 5000 },
 	}
 	const catAssets: PetAssets = {
-		collectibleIcon: LuBone,
+		collectibleIcon: LuMouse,
 		collectibleSize: 24,
 		collectibleFallSpeed: 2,
 		collectibleColor: 'gray-200',
@@ -59,7 +59,7 @@ export const CatComponent = () => {
 		dimensions,
 		assets,
 	} = useBasePetLogic({
-		name: getCurrentPetName() || 'Akita',
+		name: getCurrentPetName() || 'Zardaloo',
 		animations: catAnimations,
 		dimensions: catDimensions,
 		durations: catDurations,
@@ -68,7 +68,7 @@ export const CatComponent = () => {
 
 	return (
 		<BasePetContainer
-			name={getCurrentPetName() || 'Akita'}
+			name={getCurrentPetName() || 'Zardaloo'}
 			containerRef={containerRef}
 			petRef={petRef}
 			position={position}
@@ -78,7 +78,7 @@ export const CatComponent = () => {
 			getAnimationForCurrentAction={getAnimationForCurrentAction}
 			dimensions={dimensions}
 			assets={assets}
-			altText="Interactive Cat"
+			altText="a Dummy Cat"
 		/>
 	)
 }
