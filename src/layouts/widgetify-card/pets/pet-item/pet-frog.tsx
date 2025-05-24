3128989be7775b467c44879b32bf23224060dc80
masihgh <masihgraph@gmail.com>
2025-05-24 16:44:42 +0330

    a funny bug for bugs!

--- src/layouts/widgetify-card/pets/pet-item/pet-frog.tsx
+++ src/layouts/widgetify-card/pets/pet-item/pet-frog.tsx
@@ -1,6 +1,6 @@
 import idle from '@/assets/animals/frog/ghoori_idle_8fps.gif'
 import lie from '@/assets/animals/frog/ghoori_lie_8fps.gif'
-import running from '@/assets/animals/frog/ghoori_walk_fast_8fps.gif'
+import running from '@/assets/animals/frog/ghoori_run_8fps.gif'
 import swipe from '@/assets/animals/frog/ghoori_swipe_8fps.gif'
 import walking from '@/assets/animals/frog/ghoori_walk_8fps.gif'
 import walking_fast from '@/assets/animals/frog/ghoori_walk_fast_8fps.gif'
@@ -29,24 +29,38 @@ export const frogComponent = () => {
     }
 
     const frogDimensions: PetDimensions = {
-       size: 32,
-		walkSpeed: PetSpeed.SLOW,
-		runSpeed: PetSpeed.NORMAL,
-		climbSpeed: 0.8,
-		maxHeight: 80,
+        size: 32,
+        walkSpeed: PetSpeed.SLOW,
+        runSpeed: PetSpeed.NORMAL,
+        climbSpeed: PetSpeed.NORMAL,
+        maxHeight: 80,
     }
 
     const frogDurations: PetDurations = {
-       walk: { min: 4000, max: 9000 },
-		run: { min: 2000, max: 5000 },
-		rest: { min: 6000, max: 12000 },
-		climb: { min: 3000, max: 6000 },
+        walk: { min: 4000, max: 9000 },
+        run: { min: 2000, max: 5000 },
+        rest: { min: 6000, max: 12000 },
+        climb: { min: 3000, max: 6000 },
     }
+
+    const collectibleColors = [
+        'red-400',
+        'green-500',
+        'purple-300',
+        'yellow-200',
+        'pink-600',
+        'teal-400',
+        'cyan-500',
+        'indigo-400',
+        'lime-300',
+        'blue-500'
+    ];
+
     const frogAssets: PetAssets = {
         collectibleIcon: LuBug,
         collectibleSize: 24,
         collectibleFallSpeed: 2,
-        collectibleColor: 'blue-300',
+        collectibleColor: collectibleColors[Math.floor(Math.random() * collectibleColors.length)],
     }
 
     const {
