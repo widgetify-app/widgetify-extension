c04dfdc722debc66e9a6fe28d813370cec8a588e
ali0sam <ugd455078@gmail.com>
2025-06-08 22:20:54 +0330

    Update navbar.layout.tsx
    fix: replace settings icon and adjust layout for better alignment

--- src/layouts/navbar/navbar.layout.tsx
+++ src/layouts/navbar/navbar.layout.tsx
@@ -3,7 +3,7 @@ import Tooltip from '@/components/toolTip'
 import { useWidgetVisibility } from '@/context/widget-visibility.context'
 import { type JSX, useEffect, useState } from 'react'
 import { TbApps } from 'react-icons/tb'
-import { VscSettings } from 'react-icons/vsc'
+import { CiSettings } from "react-icons/ci";
 import { SettingModal } from '../setting/setting-modal'
 import { FriendsList } from './friends-list/friends'
 import { SyncButton } from './sync/sync'
@@ -52,10 +52,10 @@ export function NavbarLayout(): JSX.Element {
 					</Tooltip>
 					<Tooltip content="تنظیمات">
 						<div
-							className="flex items-center w-8 h-8 gap-2 px-2 overflow-hidden transition-all border cursor-pointer border-content rounded-xl bg-content backdrop-blur-sm hover:opacity-80 hover:scale-105"
+							className="flex items-center justify-center w-8 h-8 gap-2 px-1 overflow-hidden transition-all border cursor-pointer border-content rounded-xl bg-content backdrop-blur-sm hover:opacity-80 hover:scale-105"
 							onClick={() => setShowSettings(true)}
 						>
-							<VscSettings size={18} className="text-muted" />
+							<CiSettings size={22} className="text-muted" />
 						</div>
 					</Tooltip>
 				</div>
