ca3fb12c4b390026667a636996c33326bef907b8
sajjad isvand <sajjadzibaee102@gmail.com>
2025-06-27 00:43:15 +0330

    feat(DateDisplay): enhance date display with holiday visual indicators

--- src/layouts/widgets/wigiPad/date-display/date.display.tsx
+++ src/layouts/widgets/wigiPad/date-display/date.display.tsx
@@ -8,32 +8,44 @@ export function DateDisplay() {
 	const isHoliday = sortedEvents.some((event) => event.isHoliday) || todayIsHoliday
 	const isDayTime = today.hour() >= 6 && today.hour() < 18
 
+	const textColor = isHoliday
+		? 'text-error drop-shadow-md'
+		: isDayTime
+			? 'text-content drop-shadow-md'
+			: 'text-primary drop-shadow-sm'
+
 	return (
 		<div
 			className={
-				'relative flex flex-col items-center justify-center gap-0.5 p-1 overflow-hidden text-center border border-b-0 border-l-0 rounded-2xl border-content bg-widget widget-wrapper transition-all duration-500'
+				'relative flex flex-col items-center justify-center gap-0.5 p-1 overflow-hidden text-center  rounded-2xl border-content bg-widget widget-wrapper transition-all duration-500'
 			}
 		>
-			<span className={`text-base !leading-none ${
-				isHoliday
-					? 'text-error drop-shadow-md'
-						: 'text-content drop-shadow-md'
-				}`}>
-					{today.locale('fa').format('dddd')}
+			{isHoliday && (
+				<>
+					<div className="absolute w-20 px-2 py-1 text-xs text-white transform -rotate-45 shadow-xl right-16 top-2 bg-error">
+						<div className="relative z-10 font-semibold tracking-wide">
+							تعطیل
+						</div>
+						<div className="absolute inset-0 opacity-50 bg-error/80 blur-sm" />
+					</div>
+					<div className="absolute inset-0 pointer-events-none bg-gradient-to-br from-error/5 via-transparent to-error/10" />
+					<div className="absolute w-2 h-2 rounded-full top-1 left-1 bg-error/30 animate-pulse" />
+					<div className="absolute bottom-1 right-1 w-1.5 h-1.5 bg-error/20 rounded-full animate-pulse delay-300" />{' '}
+				</>
+			)}
+
+			<span className={`text-base !leading-none ${textColor}`}>
+				{today.locale('fa').format('dddd')}
 			</span>
 			<div
-				className={`text-7xl !leading-none font-sans font-bold transition-all duration-300 transform ${
-					isHoliday
-						? 'text-error drop-shadow-md'
-						: isDayTime
-							? 'text-content drop-shadow-md'
-							: 'text-primary drop-shadow-sm'
-				}`}
+				className={`text-7xl !leading-none font-sans font-bold transition-all duration-300 transform ${textColor}`}
 			>
 				{today.jDate()}
 			</div>
-			<div className="text-lg font-medium transition-all duration-200">
-				{today.locale('fa').format('MMMM')}
+			<div
+				className={`text-lg font-medium transition-all duration-200 ${textColor}`}
+			>
+				{today.locale('fa').format('MMMM YYYY')}
 			</div>
 		</div>
 	)
