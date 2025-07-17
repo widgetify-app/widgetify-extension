ce75a30a49e755d85deb7d97409c784bb5805a8f
Mohammad Mohagheghian <vito.mohagheghian@gmail.com>
2025-07-17 15:45:21 +0330

    fix security issue editing folder with password, and refactor and the bookmarks file

--- src/layouts/bookmark/components/modal/folder-password.modal.tsx
+++ src/layouts/bookmark/components/modal/folder-password.modal.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/button/button'
 import Modal from '@/components/modal'
 import { TextInput } from '@/components/text-input'
+import { useEffect, useRef, useState } from 'react'
 
 interface FolderPasswordModalProps {
 	isOpen: boolean
@@ -17,6 +18,15 @@ export function FolderPasswordModal({
 }: FolderPasswordModalProps) {
 	const [password, setPassword] = useState('')
 	const [errorMessage, setErrorMessage] = useState('')
+	const inputRef = useRef<HTMLInputElement>(null)
+
+	useEffect(() => {
+		if (isOpen) {
+			setTimeout(() => {
+				inputRef.current?.focus()
+			}, 0)
+		}
+	}, [isOpen])
 
 	const invalidPassword = 'رمز عبور اشتباه است. دوباره امتحان کنید'
 
@@ -32,35 +42,41 @@ export function FolderPasswordModal({
 			}}
 		>
 			<label htmlFor="password">رمز عبور پوشه را وارد کنید</label>
-			<div className="flex items-end gap-2 pb-[7px]">
-				<TextInput
-					type="password"
-					name="password"
-					id="password"
-					placeholder="رمز عبور"
-					value={password}
-					onChange={(v) => setPassword(v)}
-					className={`mt-2 w-full px-4 py-3 text-right rounded-lg transition-all duration-300 ${errorMessage && 'border-error'}`}
-				/>
-				<Button
-					size="md"
-					isPrimary={true}
-					disabled={!password}
-					onClick={() => {
-						if (folderPassword === password) {
-							onConfirm()
-							onClose()
-							setPassword('')
-							setErrorMessage('')
-						} else setErrorMessage(invalidPassword)
-					}}
-					className={
-						'btn btn-circle !w-fit px-4 border-none shadow-none disabled:text-gray-400 rounded-xl transition-colors duration-300 ease-in-out'
-					}
-				>
-					تأیید و ورود
-				</Button>
-			</div>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault()
+					if (folderPassword === password) {
+						onConfirm()
+						onClose()
+						setPassword('')
+						setErrorMessage('')
+					} else setErrorMessage(invalidPassword)
+				}}
+			>
+				<div className="flex items-end gap-2 pb-[7px]">
+					<TextInput
+						type="password"
+						name="password"
+						id="password"
+						placeholder="رمز عبور"
+						value={password}
+						onChange={(v) => setPassword(v)}
+						className={`mt-2 w-full px-4 py-3 text-right rounded-lg transition-all duration-300 ${errorMessage && 'border-error'} focus:bg-red-400`}
+						ref={inputRef}
+					/>
+					<Button
+						size="md"
+						isPrimary={true}
+						disabled={!password}
+						type="submit"
+						className={
+							'btn btn-circle !w-fit px-4 border-none shadow-none disabled:text-gray-400 rounded-xl transition-colors duration-300 ease-in-out'
+						}
+					>
+						تأیید و ورود
+					</Button>
+				</div>
+			</form>
 			{errorMessage && (
 				<p className="text-error font-semibold text-[13px] font-[Vazir]">
 					{errorMessage}
