1342eb8f7656ec4a7202583bf463825df8e3e14c
sajjad isvand <sajjadzibaee102@gmail.com>
2025-07-22 18:54:40 +0330

    refactor(tools): remove events tab

--- src/layouts/widgets/tools/tools.layout.tsx
+++ src/layouts/widgets/tools/tools.layout.tsx
@@ -1,34 +1,18 @@
 import { motion } from 'framer-motion'
 import type React from 'react'
 import { useState } from 'react'
-
 import { getFromStorage, setToStorage } from '@/common/storage'
-import { useAuth } from '@/context/auth.context'
 import { useDate } from '@/context/date.context'
-import { useGetEvents } from '@/services/hooks/date/getEvents.hook'
-import { useGetGoogleCalendarEvents } from '@/services/hooks/date/getGoogleCalendarEvents.hook'
 import { WidgetContainer } from '../widget-container'
 import { TabNavigation } from './components/tab-navigation'
-import { Events } from './events/event'
 import { PomodoroTimer } from './pomodoro/pomodoro-timer'
 import { ReligiousTime } from './religious/religious-time'
 
-export type ToolsTabType = 'events' | 'pomodoro' | 'religious-time'
+export type ToolsTabType = 'pomodoro' | 'religious-time'
 
 export const ToolsLayout: React.FC<any> = () => {
 	const [activeTab, setActiveTab] = useState<ToolsTabType | null>(null)
-	const { selectedDate, setCurrentDate } = useDate()
-	const { data: events } = useGetEvents()
-	const { user } = useAuth()
-
-	const startOfMonth = selectedDate.clone().startOf('jMonth').toDate()
-	const endOfMonth = selectedDate.clone().endOf('jMonth').toDate()
-
-	const { data: googleEvents } = useGetGoogleCalendarEvents(
-		user?.connections?.includes('google') || false,
-		startOfMonth,
-		endOfMonth
-	)
+	const { selectedDate } = useDate()
 
 	const onTabClick = (tab: ToolsTabType) => {
 		if (tab === activeTab) return
@@ -57,22 +41,6 @@ export const ToolsLayout: React.FC<any> = () => {
 					onTabClick={onTabClick || (() => {})}
 				/>
 			</div>
-
-			{activeTab === 'events' && (
-				<motion.div
-					key="events-view"
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					exit={{ opacity: 0 }}
-				>
-					<Events
-						events={events || []}
-						googleEvents={googleEvents || []}
-						currentDate={selectedDate}
-						onDateChange={setCurrentDate}
-					/>
-				</motion.div>
-			)}
 
 			{activeTab === 'religious-time' && (
 				<motion.div
