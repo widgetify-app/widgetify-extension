1c18bd60cd4bad706e55bbd63e76a40c8837ff13
sajjad isvand <sajjadzibaee102@gmail.com>
2025-09-16 21:38:22 +0330

    update version to 1.0.23

--- wxt.config.ts
+++ wxt.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
 		'@wxt-dev/module-react',
 	],
 	manifest: {
-		version: '1.0.22',
+		version: '1.0.23',
 		name: 'Widgetify',
 		description:
 			'Transform your new tab into a smart dashboard with Widgetify! Get currency rates, crypto prices, weather & more.',
