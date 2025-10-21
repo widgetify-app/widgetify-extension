a36bcb19bcbd8ee6b6edebe666ae0cf1c975bae0
sajjad isvand <sajjadzibaee102@gmail.com>
2025-10-21 13:10:49 +0330

    fix: refactor VerticalTabsApp layout and improve FavoriteSites rendering logic

--- entrypoints/sidepanel/VerticalTabsApp.tsx
+++ entrypoints/sidepanel/VerticalTabsApp.tsx
@@ -1,16 +1,42 @@
+import { HiOutlinePlusCircle } from 'react-icons/hi'
+import { TextInput } from '@/components/text-input'
+import { safeAwait } from '@/services/api'
 import { FavoriteProvider } from './context/favorite.context'
 import { FavoriteSites } from './layouts/favorite/favorite-sites'
 import { TabsList } from './layouts/tabs/tabs-list'
 
 export default function VerticalTabsApp() {
+	const handleNewTab = async () => {
+		await safeAwait(browser.tabs.create({}))
+	}
+
 	return (
 		<FavoriteProvider>
-			<div className="min-h-screen text-white" dir="rtl">
-				<FavoriteSites />
+			<div className="flex flex-col min-h-screen text-white" dir="rtl">
+				<div className="flex-1">
+					<FavoriteSites />
 
-				<div className="mt-2 border-t border-content">
-					<TabsList />
+					<div className="mt-2 ">
+						<TabsList />
+					</div>
 				</div>
+
+				<div className="w-56 p-2">
+					<TextInput
+						onChange={() => {}}
+						value=""
+						placeholder="جستجو در وب..."
+					/>
+				</div>
+				<footer className="sticky bottom-0 border-t border-content">
+					<button
+						onClick={handleNewTab}
+						className="w-full flex items-center gap-1 justify-center p-4 text-sm font-medium transition-colors hover:bg-base-300 text-content hover:text-primary cursor-pointer text-muted hover:!text-content"
+					>
+						<HiOutlinePlusCircle />
+						تب جدید
+					</button>
+				</footer>
 			</div>
 		</FavoriteProvider>
 	)
--- entrypoints/sidepanel/layouts/favorite/favorite-sites.tsx
+++ entrypoints/sidepanel/layouts/favorite/favorite-sites.tsx
@@ -1,31 +1,34 @@
+import Analytics from '@/analytics'
 import Tooltip from '@/components/toolTip'
 import { useFavoriteStore } from '../../context/favorite.context'
+import type { FavoriteSite } from './favorite.types'
 
 export function FavoriteSites() {
 	const { favorites } = useFavoriteStore()
 
 	if (favorites.length === 0) return null
-
+	const onClick = (item: FavoriteSite) => {
+		Analytics.event('vertical_tabs_favorite_site_clicked')
+		browser.tabs.create({ url: item.url })
+	}
 	return (
 		<div className="p-4">
-			<div className="grid grid-cols-5 gap-x-3 gap-y-2 sm:grid-cols-8">
-				{/* Empty slots */}
-				{favorites.map((item, index) => {
-					return (
-						<div
-							key={`empty-${index}`}
-							className="flex items-center justify-center w-8 h-8 p-1 transition-all rounded-md cursor-pointer hover:bg-base-300"
-						>
-							<Tooltip content={item.title}>
-								<img
-									src={item.favicon}
-									alt={item.title}
-									className="object-cover w-full h-full rounded-md"
-								/>
-							</Tooltip>
-						</div>
-					)
-				})}
+			<div className="grid grid-cols-5 gap-4 justify-items-center">
+				{[...favorites, ...favorites].map((item, index) => (
+					<div
+						key={`fav-${index}`}
+						className="flex items-center justify-center w-full h-12 p-2 transition-all rounded-lg cursor-pointer bg-base-300 hover:bg-base-200 aspect-square"
+						onClick={() => onClick(item)}
+					>
+						<Tooltip content={item.title}>
+							<img
+								src={item.favicon}
+								alt={item.title}
+								className="object-cover w-full h-full rounded-md "
+							/>
+						</Tooltip>
+					</div>
+				))}
 			</div>
 		</div>
 	)
