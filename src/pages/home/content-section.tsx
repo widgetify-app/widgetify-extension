985a1c7030d4c598842acf36ab57bae3e18b0186
sajjad isvand <sajjadzibaee102@gmail.com>
2025-10-29 15:31:07 +0330

    fix: remove unnecessary overflow-y-auto class from ContentSection layout

--- src/pages/home/content-section.tsx
+++ src/pages/home/content-section.tsx
@@ -119,7 +119,7 @@ export function ContentSection() {
 			<TodoProvider>
 				<div
 					data-tour="content"
-					className={`flex flex-col items-center ${layoutPositions[contentAlignment]} flex-1 w-full gap-2 px-2 md:px-4 py-2  overflow-y-auto hide-scrollbar`}
+					className={`flex flex-col items-center ${layoutPositions[contentAlignment]} flex-1 w-full gap-2 px-2 md:px-4 py-2`}
 				>
 					<div className="flex flex-col w-full gap-4 lg:flex-row lg:gap-2">
 						<div className="order-3 w-full lg:w-1/4 lg:order-1">
