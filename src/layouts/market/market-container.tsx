37c38696e44b0e6de50544b789335b564f637897
sajjad isvand <sajjadzibaee102@gmail.com>
2025-11-14 23:55:08 +0330

    feat: add tabPosition prop to TabManager and update MarketContainer to utilize it

--- src/layouts/market/market-container.tsx
+++ src/layouts/market/market-container.tsx
@@ -1,43 +1,32 @@
-import { useState } from 'react'
+import { FaPaintBrush } from 'react-icons/fa'
+import { FaPhotoFilm } from 'react-icons/fa6'
+import { type TabItem, TabManager } from '@/components/tab-manager'
 import { MarketWallpaper } from './marketWallpaper'
 import { MarketOtherItems } from './other-items'
 
-type MarketTabType = 'wallpapers' | 'other'
-
 export function MarketContainer() {
-	const [activeTab, setActiveTab] = useState<MarketTabType>('other')
-
-	const getTabStyle = (isActive: boolean) => {
-		if (isActive) {
-			return 'border-primary/80 text-primary font-semibold border-b-2'
-		}
-		return 'border-transparent text-content hover:!text-primary'
-	}
+	const tabs: TabItem[] = [
+		{
+			label: 'شخصی‌سازی',
+			value: 'other',
+			icon: <FaPaintBrush />,
+			element: <MarketOtherItems />,
+		},
+		{
+			label: 'تصویر زمینه‌ها',
+			value: 'wallpapers',
+			icon: <FaPhotoFilm />,
+			element: <MarketWallpaper />,
+		},
+	]
 
 	return (
-		<div className="space-y-4 h-[70vh] max-h-[70vh] min-h-[70vh] px-2">
-			<div className="border-gray-200 dark:border-gray-700">
-				<ul className="flex flex-wrap text-sm font-medium text-center">
-					<li className="mr-2">
-						<button
-							onClick={() => setActiveTab('other')}
-							className={`inline-block cursor-pointer transition-all duration-200 p-2 border-b-2 rounded-t-lg ${getTabStyle(activeTab === 'other')}`}
-						>
-							شخصی‌سازی
-						</button>
-					</li>
-					<li className="mr-2">
-						<button
-							onClick={() => setActiveTab('wallpapers')}
-							className={`inline-block cursor-pointer transition-all duration-200 p-2 border-b-2 rounded-t-lg ${getTabStyle(activeTab === 'wallpapers')}`}
-						>
-							تصویر زمینه‌ها
-						</button>
-					</li>
-				</ul>
-			</div>
-
-			{activeTab === 'wallpapers' ? <MarketWallpaper /> : <MarketOtherItems />}
-		</div>
+		<TabManager
+			tabOwner="user"
+			tabs={tabs}
+			defaultTab="other"
+			direction="rtl"
+			tabPosition="top"
+		/>
 	)
 }
