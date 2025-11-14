ffeaff8c5e14aa559baa745f2b5cc4c521ab117c
sajjad isvand <sajjadzibaee102@gmail.com>
2025-11-15 03:20:08 +0330

    feat: refine browser title preview styling by removing unnecessary font-light class

--- src/components/market/title/title-render-preview.tsx
+++ src/components/market/title/title-render-preview.tsx
@@ -8,19 +8,17 @@ export function renderBrowserTitlePreview(item: Prop) {
 	const isPersian = /[\u0600-\u06FF]/.test(item.template)
 	return (
 		<div
-			className={`flex items-center justify-between w-40 p-1.5 text-xs font-medium border shadow-sm text-content bg-content border-content rounded-t-md max-w-40 ${item.className || ''}`}
+			className={`flex items-center justify-between w-40 p-1.5 text-xs font-medium border shadow-sm text-content bg-content border-content rounded-t-md max-w-40 ${item.className || ''} font-sans`}
 		>
 			<div className="flex items-center gap-1">
 				<IoMdClose />
 				{isPersian && (
-					<span className="text-[10px] font-light truncate">
-						{item.template}
-					</span>
+					<span className="text-[10px] truncate">{item.template}</span>
 				)}
 			</div>
 			<div className="flex items-center gap-1">
 				{!isPersian && (
-					<span className="text-[10px] font-light truncate" dir="ltr">
+					<span className="text-[10px] truncate" dir="ltr">
 						{item.template}
 					</span>
 				)}
