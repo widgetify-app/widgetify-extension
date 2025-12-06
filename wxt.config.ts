efee1f00584e622d4d6df02bd15a409ab7d51de5
sajjad isvand <sajjadzibaee102@gmail.com>
2025-12-07 00:27:46 +0330

    chore: bump version to 1.0.58 in configuration

--- wxt.config.ts
+++ wxt.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
 		'@wxt-dev/module-react',
 	],
 	manifest: {
-		version: '1.0.57',
+		version: '1.0.58',
 		name: 'Widgetify',
 		description:
 			'Transform your new tab into a smart dashboard with Widgetify! Get currency rates, crypto prices, weather & more.',
