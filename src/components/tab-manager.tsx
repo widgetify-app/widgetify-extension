25a1a5f7ddb1c8d077dcc63548887a2048840b13
Shak <sanshakurt@gmail.com>
2025-12-10 12:56:35 +0330

    refactor: add children prop to TabManager for additional tab buttons

--- src/components/tab-manager.tsx
+++ src/components/tab-manager.tsx
@@ -17,6 +17,7 @@ interface TabManagerProps {
 	onTabChange?: (tabValue: string) => void
 	direction?: 'rtl' | 'ltr'
 	tabPosition?: 'top' | 'side'
+	children?: ReactNode // for additional tab buttons at the end of aside (just pass the actual buttons)
 }
 
 export const TabManager = ({
@@ -27,6 +28,7 @@ export const TabManager = ({
 	direction = 'rtl',
 	tabOwner,
 	tabPosition,
+	children,
 }: TabManagerProps) => {
 	const [activeTab, setActiveTab] = useState(defaultTab || tabs[0]?.value || '')
 	const contentRef = useRef<HTMLDivElement>(null)
@@ -69,28 +71,38 @@ export const TabManager = ({
 			? 'shrink-0 md:overflow-y-auto w-full'
 			: 'md:flex-col md:w-48 shrink-0 md:overflow-y-auto'
 	return (
-		<div dir={direction} className={`flex ${headClass}  overflow-hidden`}>
-			<div
-				className={`flex w-full gap-2 p-2 overflow-x-auto rounded-lg ${contentClass}`}
-			>
-				{tabs.map(({ label, value, icon, isNew }) => (
-					<button
-						key={value}
-						onClick={() => handleTabChange(value)}
-						className={`relative flex items-center gap-3 px-4 py-3 rounded-full transition-all duration-200 ease-in-out justify-start cursor-pointer whitespace-nowrap active:scale-[0.98] ${getTabButtonStyle(activeTab === value)}`}
-					>
-						<span
-							className={`relative ${getTabIconStyle(activeTab === value)}`}
+		<div dir={direction} className={`flex ${headClass}   overflow-hidden`}>
+			<aside className="md:h-full flex justify-between flex-col  overflow-hidden">
+				<div
+					className={`flex w-full gap-2 p-2 overflow-x-auto rounded-lg ${contentClass}`}
+				>
+					{tabs.map(({ label, value, icon, isNew }) => (
+						<button
+							key={value}
+							onClick={() => handleTabChange(value)}
+							className={`relative flex items-center gap-3 px-4 py-3 rounded-full transition-all duration-200 ease-in-out justify-start cursor-pointer whitespace-nowrap active:scale-[0.98] ${getTabButtonStyle(activeTab === value)}`}
 						>
-							{icon}
-							{isNew && (
-								<span className="absolute left-0 z-30 w-2 h-2 duration-200 rounded-full -bottom-1 bg-error animate-ping"></span>
-							)}
-						</span>
-						<span className="text-sm">{label}</span>
-					</button>
-				))}
-			</div>{' '}
+							<span
+								className={`relative ${getTabIconStyle(activeTab === value)}`}
+							>
+								{icon}
+								{isNew && (
+									<span className="absolute left-0 z-30 w-2 h-2 duration-200 rounded-full -bottom-1 bg-error animate-ping"></span>
+								)}
+							</span>
+							<span className="text-sm">{label}</span>
+						</button>
+					))}
+					{children}
+				</div>
+				<div
+					aria-label="bottom tab buttons container"
+					className={`md:flex w-full gap-2 p-2 overflow-x-auto rounded-lg ${contentClass} hidden`}
+				>
+					{children}
+				</div>
+			</aside>
+
 			<div
 				className="relative flex-1 overflow-x-hidden overflow-y-auto rounded-lg"
 				ref={contentRef}
