3ccfde7d58617add1433c10864e4cd886dadf23e
sajjad isvand <sajjadzibaee102@gmail.com>
2026-05-05 13:36:29 +0330

    fix

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -22,7 +22,6 @@ import { useAppearanceSetting } from '@/context/appearance.context'
 import { HomeContentSimplify } from './home/home-content-simplify'
 import { AuthRequiredModal } from '@/components/auth/AuthRequiredModal'
 import { UpdateChecker } from '@/components/updater/updater'
-import { MiniAppsLayout } from '@/layouts/mini-apps/mini-apps.layout'
 import { MiniAppPage } from './mini-apps/mini-app.page'
 
 const steps: Step[] = [
