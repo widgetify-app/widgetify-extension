98ba6ebe09438311fb389c744e81a00a0a840c9c
sajjad isvand <sajjadzibaee102@gmail.com>
2026-06-07 10:51:21 +0330

    remove pet from wigipad

--- src/layouts/simplify/wigipad-simplify.tsx
+++ src/layouts/simplify/wigipad-simplify.tsx
@@ -30,8 +30,8 @@ export function SimplifyYadkar() {
 				<ClockDisplay />
 			</div>
 
-			<div className="col-span-2 px-1 mt-1 mb-1">
-				<div className="overflow-y-auto scrollbar-none max-h-30 min-h-30">
+			<div className="col-span-2 px-1 mt-1">
+				<div className="overflow-y-auto scrollbar-none max-h-36 min-h-36">
 					<div className="pb-8">
 						{fetchedData?.wigiPad?.map((notification, index) => (
 							<RenderWigiPadItem
@@ -43,59 +43,6 @@ export function SimplifyYadkar() {
 					</div>
 				</div>
 			</div>
-			<PetProvider>
-				<PetRender />
-			</PetProvider>
 		</WidgetContainer>
 	)
 }
-function PetRender() {
-	const { isEnabled } = usePetContext()
-	if (!isEnabled) return null
-
-	return (
-		<div className="z-10 w-full h-6 -mb-5 overflow-hidden">
-			<Pet />
-		</div>
-	)
-}
-
-// export function SimplifyYadkar() {
-// 	const [activeSection, setActiveSection] = useState<string>('all')
-// 	const tabContainerRef = useRef<HTMLDivElement>(null)
-
-// 	const onChangeTab = (val: string) => {
-// 		setActiveSection(val)
-// 		Analytics.event(`wigipad_simplify_tab_${val}`)
-// 	}
-
-// 	const renderContent = () => {
-// 		switch (activeSection) {
-// 			case 'weather':
-// 				return <InfoWeather />
-// 			case 'pomodoro':
-// 				return <PomodoroTimer />
-
-// 			default:
-// 				return (
-// 					<div className="grid grid-cols-2 grid-rows-2 gap-x-2">
-// 						<DateDisplay />
-// 						<ClockDisplay />
-// 						<div className="col-span-2">
-// 							<div className="overflow-y-auto scrollbar-none max-h-32 min-h-32">
-// 								<div className="pb-2 mt-2">
-// 									<NotificationCenter />
-// 								</div>
-// 							</div>
-// 						</div>
-// 					</div>
-// 				)
-// 		}
-// 	}
-
-// 	return (
-// 		<WidgetContainer className="flex flex-col">
-// 			<div className="relative flex-1 h-60">{renderContent()}</div>
-// 		</WidgetContainer>
-// 	)
-// }
