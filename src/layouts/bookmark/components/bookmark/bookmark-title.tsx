1700ad60effd71044a586b87b44cbb94d224fde6
sajjad isvand <sajjadzibaee102@gmail.com>
2026-07-04 12:57:54 +0330

    fix(bookmark): limit title width to prevent overflow issues

--- src/layouts/bookmark/components/bookmark/bookmark-title.tsx
+++ src/layouts/bookmark/components/bookmark/bookmark-title.tsx
@@ -7,7 +7,7 @@ export function BookmarkTitle({
 	customTextColor?: string
 }) {
 	return (
-		<div className="w-full px-1 text-center truncate" dir="auto">
+		<div className="px-1 text-center truncate w-22 max-w-22" dir="auto">
 			<span
 				style={{ color: customTextColor || undefined, zIndex: 10 }}
 				className={`text-[.65rem] sm:text-[.7rem] md:text-[.75rem] font-medium transition-colors duration-300 opacity-85 block truncate ${!customTextColor && 'text-content'} group-hover:opacity-100`}
