296edee42b755c91b9f00abdd883afb9e4fff5cf
sajjad isvand <sajjadzibaee102@gmail.com>
2026-09-05 10:08:40 +0330

    feat: add react-scan for development logging and monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { scan } from 'react-scan'
 import './fonts.css'
 import './index.css'
 import App from './app'
+
+if (import.meta.env.DEV) {
+	scan({
+		enabled: true,
+		log: true,
+	})
+}
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
