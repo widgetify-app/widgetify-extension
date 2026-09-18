807763a2c4af097ec0d272f121d6cadcad9ead04
sajjad isvand <sajjadzibaee102@gmail.com>
2026-09-19 00:25:48 +0330

    fix(fonts): load remote fonts asynchronously to improve performance

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,20 @@ import './fonts.css'
 import './index.css'
 import App from './app'
 
+function loadRemoteFontsAsync() {
+	if (typeof document === 'undefined') return
+	const id = 'widgetify-remote-fonts'
+	if (document.getElementById(id)) return
+
+	const link = document.createElement('link')
+	link.id = id
+	link.rel = 'stylesheet'
+	link.href = 'https://cdn.widgetify.ir/fonts/remote-fonts.css'
+	document.head.appendChild(link)
+}
+
+loadRemoteFontsAsync()
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
